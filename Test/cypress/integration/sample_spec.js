486e04e95448c803f3a6136b667bd03accace31e
Polskig <kimjankowski@outlook.com>
2022-02-14 14:11:08 +0100

    test

---
+++ Test/cypress/integration/sample_spec.js
@@ -0,0 +1,17 @@
+describe('My First Test', function () {
+    it('Clicks an element', function () {
+        cy.viewport('iphone-6')
+
+        cy.visit('https://www.inet.se/')
+
+        cy.contains('Jag förstår').click()
+
+        cy.contains('Datorer').click()
+
+        cy.url().should('include', '/kategori/27/datorer')
+
+        cy.get('.form-control').type('MSI').should('have.value', 'MSI')
+
+        cy.contains('Sök').click()
+    })
+})
