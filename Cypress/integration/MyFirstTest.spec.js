c0e692fa4f480c26f345057ace266848ce9576e6
Polskig <kimjankowski@outlook.com>
2022-02-21 10:36:56 +0100

    Update MyFirstTest.spec.js
    Adding so there are 5 basics tests in the file

--- Cypress/integration/MyFirstTest.spec.js
+++ Cypress/integration/MyFirstTest.spec.js
@@ -13,6 +13,24 @@ describe('My First Test', function () {
         cy.contains('Sök').click()
     })
 
+    it('add something to the cart', function () {
+        //cy.viewport('iphone-6')
+
+        cy.visit('https://www.inet.se/')
+
+        cy.contains('Jag förstår').click()
+
+        cy.get('.form-control').type('MSI').should('have.value', 'MSI')
+
+        cy.contains('Sök').click()
+
+        cy.contains('MSI 27" Optix G273QF QHD IPS 165 Hz').click()
+
+        cy.get('.product-purchase > .box-body > .btn').click()
+
+        //cy.get('.product-name > a').should('MSI 27" Optix G273QF QHD IPS 165 Hz')
+    })
+
     it('add something to the cart on a phone', function () {
         cy.viewport('iphone-6')
 
@@ -29,5 +47,61 @@ describe('My First Test', function () {
         cy.get('.product-purchase > .box-body > .btn').click()
 
         //cy.get('.product-name > a').should('MSI 27" Optix G273QF QHD IPS 165 Hz')
+    })
+
+    it('Clicks thge next and previous on slides', function () {
+        //cy.viewport('iphone-6')
+
+        cy.visit('https://www.inet.se/')
+
+        cy.contains('Jag förstår').click()
+
+        cy.get('.next > .fa').click()
+
+        cy.wait(1000)
+
+        cy.get('.next > .fa').click()
+
+        cy.wait(1000)
+
+        cy.get('.next > .fa').click()
+
+        cy.wait(1000)
+
+        cy.get('.prev > .fa').click()
+
+        cy.wait(1000)
+
+        cy.get('.prev > .fa').click()
+
+        cy.wait(1000)
+
+        cy.get('.prev > .fa').click()
+    })
+
+    it('Clicks an element', function () {
+        //cy.viewport('iphone-6')
+
+        cy.visit('https://www.inet.se/')
+
+        cy.contains('Jag förstår').click()
+
+        cy.get('.form-control').type('MSI').should('have.value', 'MSI')
+
+        cy.contains('Sök').click()
+
+        cy.contains('MSI 27" Optix G273QF QHD IPS 165 Hz').click()
+
+        cy.get('.product-purchase > .box-body > .btn').click()
+
+        cy.get('.qty-selector > :nth-child(3)').click()
+
+        cy.get('.qty-selector > :nth-child(3)').click()
+
+        cy.get('.qty-selector > :nth-child(1)').click()
+
+        cy.get('.qty-selector > :nth-child(1)').click()
+
+        //cy.get('.qty-selector > :nth-child(1)').click()
     })
 })
