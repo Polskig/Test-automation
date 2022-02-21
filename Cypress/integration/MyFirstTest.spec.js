af3fc87c08ffd6da46db24cde126fbb30babaaf1
Polskig <kimjankowski@outlook.com>
2022-02-21 10:30:47 +0100

    Update MyFirstTest.spec.js
    Start adding tests to one file

--- Cypress/integration/MyFirstTest.spec.js
+++ Cypress/integration/MyFirstTest.spec.js
@@ -12,4 +12,22 @@ describe('My First Test', function () {
 
         cy.contains('Sök').click()
     })
+
+    it('add something to the cart on a phone', function () {
+        cy.viewport('iphone-6')
+
+        cy.visit('https://www.inet.se/')
+
+        cy.contains('Jag förstår').click()
+
+        cy.get('.form-control').type('MSI').should('have.value', 'MSI')
+
+        cy.contains('Sök').click()
+
+        cy.contains('MSI 27" Optix G273QF QHD IPS 165 Hz').click()
+
+        cy.get('.product-purchase > .box-body > .btn').click()
+
+        //cy.get('.product-name > a').should('MSI 27" Optix G273QF QHD IPS 165 Hz')
+    })
 })
